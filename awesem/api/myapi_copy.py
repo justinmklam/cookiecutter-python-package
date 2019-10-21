f4b7791bb7e1f86993a1d3824582b429383df1f6
Justin Lam <justin@mistywest.com>
2019-10-21 15:56:04 -0700

    Add another dummy function to check documentation

---
+++ awesem/api/myapi_copy.py
@@ -0,0 +1,19 @@
+"""
+My API Copy
+============
+
+Just a test to make sure Sphinx Docs works
+
+"""
+
+def test_function_copy(a:int, b:int) -> int:
+    """Adds two numbers together
+
+    Args:
+        a (int): Number 1
+        b (int): Number 2
+
+    Returns:
+        int: Sum of both numbers
+    """
+    return a + b
