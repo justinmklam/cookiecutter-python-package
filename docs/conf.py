db49bb9e6547851650de994880f245e3a638c56e
Justin Lam <justinmklam@gmail.com>
2019-12-27 11:37:28 -0800

    Use index.rst instead of contents.rst as master doc

--- docs/conf.py
+++ docs/conf.py
@@ -42,6 +42,9 @@ napoleon_use_ivar = True
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+# The master toctree document.
+master_doc = 'index'
+
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 source_suffix = [".rst", ".md"]
