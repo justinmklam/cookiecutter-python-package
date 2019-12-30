a693757c4768a492d9048d1519fa07c41f807abf
Justin Lam <justinmklam@gmail.com>
2019-12-30 13:40:35 -0800

    Add back extension for google style docstring compatability

--- {{cookiecutter.package_name}}/docs/conf.py
+++ {{cookiecutter.package_name}}/docs/conf.py
@@ -32,9 +32,10 @@ release = '{{cookiecutter.package_version}}'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.todo',
-    'sphinx.ext.viewcode',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
     "sphinx_rtd_theme",
     "recommonmark"
 ]
